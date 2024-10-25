0432f73514720bd8946bb2fa671a645f25af9f4a
Gerard Adamiak <134928600+GerardAdamiak@users.noreply.github.com>
2024-10-25 12:56:30 +0200

    home page updating

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,30 @@
-// Home.js
 import React from 'react';
 
 function Home({ language }) {
-  React.useEffect(() => {
-    document.title = language === 'PL' ? 'Strona Główna' : 'Home';
-  }, [language]);
+    React.useEffect(() => {
+        document.title = language === 'PL' ? 'Strona Główna' : 'Home';
+    }, [language]);
 
-  return (
-    <div className="App">
-      <header id="home" className="App-header">
-        <div className="hero-text-silver">
-          <h1>Kontuszowa</h1>
+    return (
+        <div className="App">
+            <header id="home" className="App-header">
+                <div className="hero-text-silver">
+                    <h1>Kontuszowa</h1>
+                </div>
+            </header>
+
+            <section className="intro-section">
+                <h2>{language === 'PL' ? 'Polska Tradycyjna Wódka Premium' : 'Premium Polish Vodka'}</h2>
+                <p>{language === 'PL' ? 'Witamy w Kontuszowej...' : 'Welcome to Kontuszowa...'}</p>
+            </section>
+
+            <section className="centered-text-with-images">
+                <img src="szlachta1.png" alt="Szlachta 1" className="szlachta-image" />
+                <h2>OBUDŹ W SOBIE SZLACHTĘ</h2>
+                <img src="szlachta2.png" alt="Szlachta 2" className="szlachta-image" />
+            </section>
         </div>
-      </header>
-
-      <section className="intro-section">
-        <h2>{language === 'PL' ? 'Polska Tradycyjna Wódka Premium' : 'Premium Polish Vodka'}</h2>
-        <p>
-          {language === 'PL'
-            ? 'Witamy w Kontuszowej, marce wykwintnej, tradycyjnie destylowanej polskiej wódki. Każda butelka to święto autentyczności.'
-            : 'Welcome to Kontuszowa, a brand of exquisite, traditionally distilled Polish vodka. Crafted with care and heritage, every bottle is a celebration of authenticity.'}
-        </p>
-      </section>
-    </div>
-  );
+    );
 }
 
 export default Home;
